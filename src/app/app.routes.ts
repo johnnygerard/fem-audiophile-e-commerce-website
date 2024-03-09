2c3d1931de095c059d43a2bea1bfbc8416381a10
Johnny Gérard <john@jgerard.dev>
2024-03-09 05:22:24 +0100

    feat: add navigation routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+@Component({
+  selector: 'app-dummy',
+  template: '<p>dummy works!</p>',
+  standalone: true,
+})
+class DummyComponent { }
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: DummyComponent,
+  },
+  {
+    path: 'headphones',
+    component: DummyComponent,
+  },
+  {
+    path: 'speakers',
+    component: DummyComponent,
+  },
+  {
+    path: 'earphones',
+    component: DummyComponent,
+  },
+  {
+    path: 'product/:id',
+    component: DummyComponent,
+  },
+  {
+    path: 'checkout',
+    component: DummyComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
