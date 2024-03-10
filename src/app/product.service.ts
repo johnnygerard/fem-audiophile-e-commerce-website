b1377f28b352f5ba1a6d85411c2dbb2475169664
Johnny Gérard <john@jgerard.dev>
2024-03-10 06:51:03 +0100

    feat: update product service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,30 +2,31 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from './types/product.type';
 import { ProductCategory } from './types/product-category.enum';
-import { Observable, map } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  readonly products = this._http.get<Product[]>('/assets/products.json');
+  readonly #products$ = new BehaviorSubject<Product[]>([]);
+  readonly products$ = this.#products$.asObservable();
 
-  constructor(private _http: HttpClient) { }
-
-  getProduct(id: number): Observable<Product> {
-    return this.products.pipe(
-      map(products => {
-        const product = products.find(product => product.id === id);
-
-        if (product) return product;
-        throw Error(`Product #${id} not found`);
-      })
-    );
+  constructor(private _http: HttpClient) {
+    this._http.get<Product[]>('/assets/products.json')
+      .subscribe(products => {
+        this.#products$.next(products);
+        this.#products$.complete();
+      });
   }
 
-  getProductsByCategory(category: ProductCategory): Observable<Product[]> {
-    return this.products.pipe(
-      map(products => products.filter(product => product.category === category))
-    );
+  getProduct(id: string): Product {
+    const product = this.#products$.value.find(product => product.id === id);
+
+    if (product) return product;
+    throw Error(`Product #${id} not found`);
+  }
+
+  getProductsByCategory(category: ProductCategory): Product[] {
+    return this.#products$.value.filter(product => product.category === category);
   }
 }
