8e75e87aad00da500789e832c4401103b6b05dd3
Johnny Gérard <john@jgerard.dev>
2024-03-11 12:23:24 +0100

    feat: add footer component

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LogoComponent } from '../svg/logo/logo.component';
+import { NavigationComponent } from '../navigation/navigation.component';
+import { SocialMediaComponent } from '../social-media/social-media.component';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [
+    LogoComponent,
+    NavigationComponent,
+    SocialMediaComponent,
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FooterComponent {
+
+}
