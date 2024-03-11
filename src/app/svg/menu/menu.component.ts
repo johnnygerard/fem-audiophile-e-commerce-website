c991296beb7e199a38585fab89de7c90e120647a
Johnny Gérard <john@jgerard.dev>
2024-03-11 01:54:12 +0100

    feat: add aria-hidden attribute to SVG element

--- src/app/svg/menu/menu.component.ts
+++ src/app/svg/menu/menu.component.ts
@@ -5,7 +5,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-<svg width="16" height="15" viewBox="0 0 16 15" fill="white">
+<svg width="16" height="15" viewBox="0 0 16 15" fill="white" aria-hidden="true">
   <rect width="16" height="3"/>
   <rect y="6" width="16" height="3"/>
   <rect y="12" width="16" height="3"/>
