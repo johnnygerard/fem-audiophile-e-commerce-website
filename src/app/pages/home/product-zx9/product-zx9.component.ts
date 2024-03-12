0557380e73f8a7f769cf38657c1be75f2b8dc50d
Johnny Gérard <john@jgerard.dev>
2024-03-12 18:22:03 +0100

    feat: add rings to product zx9 component

--- src/app/pages/home/product-zx9/product-zx9.component.ts
+++ src/app/pages/home/product-zx9/product-zx9.component.ts
@@ -2,11 +2,13 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { SourceMedia } from '../../../types/source-media.enum';
 import { PictureService } from '../../../../assets/images/picture.service';
 import { RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-product-zx9',
   standalone: true,
   imports: [
+    NgFor,
     RouterLink,
   ],
   templateUrl: './product-zx9.component.html',
