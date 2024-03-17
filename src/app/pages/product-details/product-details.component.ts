689b32e53cda2ee52b4faecaae4b547a2aae44c1
Johnny Gérard <john@jgerard.dev>
2024-03-17 20:32:35 +0100

    feat: add product details page component

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -4,14 +4,26 @@ import { map, of } from 'rxjs';
 import { Product } from '../../types/product.type';
 import { ProductService } from '../../services/product.service';
 import { AsyncPipe, NgIf } from '@angular/common';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ProductPreviewPriceComponent } from './product-preview-price/product-preview-price.component';
+import { ProductInfoComponent } from './product-info/product-info.component';
+import { RouterLink } from '@angular/router';
+import { RichNavigationComponent } from '../../shared/rich-navigation/rich-navigation.component';
+import { AboutComponent } from '../../shared/about/about.component';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
   imports: [
     NgIf,
+    RouterLink,
     AsyncPipe,
+    ProductPreviewPriceComponent,
+    ProductInfoComponent,
+    GalleryComponent,
     ProductRecommendationsComponent,
+    RichNavigationComponent,
+    AboutComponent,
   ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss',
