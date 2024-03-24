02de7036bf34e5fb0e75b50bd4866478783c9184
Johnny Gérard <john@jgerard.dev>
2024-03-24 02:59:38 +0100

    refactor: move guards and resolvers to new folders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { productDetailsTitleResolver } from './product-details-title.resolver';
-import { productCategoryPageGuard } from '../product-category-page.guard';
-import { productCategoryPageTitleResolver } from './product-category-page-title.resolver';
-import { checkoutPageGuard } from './checkout-page.guard';
+import { productDetailsTitleResolver } from './resolvers/product-details-title.resolver';
+import { productCategoryPageGuard } from './guards/product-category-page.guard';
+import { productCategoryPageTitleResolver } from './resolvers/product-category-page-title.resolver';
+import { checkoutPageGuard } from './guards/checkout-page.guard';
 
 export const PRODUCT_CATEGORY_PAGE_BASE_PATH = 'product-category';
 export const PRODUCT_CATEGORY_PAGE_PATH = `${PRODUCT_CATEGORY_PAGE_BASE_PATH}/:category`;
--- src/app/guards/product-category-page.guard.ts
+++ src/app/guards/product-category-page.guard.ts
@@ -1,6 +1,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { ProductCategory } from './app/types/product-category.enum';
+import { ProductCategory } from '../types/product-category.enum';
 
 const productCategories = Object.values(ProductCategory) as string[];
 
